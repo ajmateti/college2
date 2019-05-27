758737e245f2f8a891d4233f3ba16b8e5c388336
unknown <ajmateti@gmail.com>
2019-05-27 15:48:57 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ImagesComponent } from './home/images/images.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes =
+  [
+    {
+      path: '', component: HomeComponent
+    },
+    {
+      path: 'faculty', component: FacultyComponent
+    }
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ImagesComponent,
+    FacultyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
